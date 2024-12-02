65d2b280d3bb3098305d3b352f210c88d725a8a6
bednie <88800117+bednie@users.noreply.github.com>
2024-12-02 14:24:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const data = {
+// First template for static data
+const staticData = {
     title: "Welcome to CS3210 Template Engine Lab",
     content: "This is a simple template engine demonstration.",
     author: "Dr.Red",
@@ -12,37 +13,11 @@ function renderTemplate(template, data) {
     return template;
 }
 
-const template = `
+const staticTemplate = `
     <h3>{{title}}</h3>
     <p>{{content}}</p>
     <p>Author: {{author}}</p>
 `;
 
 const app = document.getElementById('app');
-app.innerHTML = renderTemplate(template, data);
-
-// Define a template using Mustache.js
-var template = `
-    <ul>
-        {{#data}}
-        <li>Name: {{name}}, Age: {{age}}, City: {{city}}</li>
-        {{/data}}
-    </ul>
-`;
-
-// Get the 'output' div element
-var output = document.getElementById('output');
-
-// Fetch JSON data from the file
-fetch('data.json')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        // Render the data using the template
-        var rendered = Mustache.render(template, { data: data });
-        output.innerHTML = rendered;
-    })
-    .catch(function(error) {
-        console.error('Error:', error);
-    });
+app.innerHTML = renderTemplate(staticTemplate, staticData);
