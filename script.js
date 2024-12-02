6d7d539fb70d25ea2b74110eb6bd3c189c275a9b
bednie <88800117+bednie@users.noreply.github.com>
2024-12-02 14:27:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-const staticData = {
-    title: "Welcome to CS3210 Template Engine Lab",
-    content: "This is a simple template engine demonstration.",
-    author: "Dr.Red",
-};
-
-function renderTemplate(template, data) {
-    for (const key in data) {
-        const regex = new RegExp('{{' + key + '}}', 'g');
-        template = template.replace(regex, data[key]);
-    }
-    return template;
-}
-
-const combinedTemplate = `
-    <h3>{{title}}</h3>
-    <p>{{content}}</p>
-    <p>Author: {{author}}</p>
-    <div id="jsonData">
-        <ul>
-            {{#people}}
-            <li>Name: {{name}}, Age: {{age}}, City: {{city}}</li>
-            {{/people}}
-        </ul>
-    </div>
+// Define the template using Mustache.js
+const template = `
+    <ul>
+        {{#data}}
+        <li>Name: {{name}}, Age: {{age}}, City: {{city}}</li>
+        {{/data}}
+    </ul>
 `;
 
-const app = document.getElementById('app');
+// Get the 'output' div element
+const output = document.getElementById('output');
 
-// First render the static data
-app.innerHTML = renderTemplate(combinedTemplate, staticData);
-
-// Then fetch and render the JSON data
+// Fetch JSON data from the file
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        const jsonContainer = document.getElementById('jsonData');
-        jsonContainer.innerHTML = Mustache.render(`
-            <ul>
-                {{#data}}
-                <li>Name: {{name}}, Age: {{age}}, City: {{city}}</li>
-                {{/data}}
-            </ul>
-        `, { data: data });
+        // Render the data using the template
+        const rendered = Mustache.render(template, { data: data });
+        output.innerHTML = rendered;
     })
     .catch(error => console.error('Error:', error));
